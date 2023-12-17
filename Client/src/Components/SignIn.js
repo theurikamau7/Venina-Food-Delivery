3054055d458ea3159c262807747cddc3c9bb9337
kibigo <jacksonkibigo99@gmail.com>
2023-12-17 23:47:07 +0300

    Added login, register

--- Client/src/Components/SignIn.js
+++ Client/src/Components/SignIn.js
@@ -1,69 +1,191 @@
-import React, { useState } from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button, Form } from "react-bootstrap";
-import {useNavigate} from 'react-router-dom'
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
-function SignIn(){
-    const [formData, setFormData] = useState({
-        email:"",
-        password:""
-    })
+const SignIn = () => {
+    const [username, usernameupdate] = useState('');
+    const [password, passwordupdate] = useState('');
 
-    const handleChange = (event) => {
-        const key = event.target.id;
-        const value = event.target.value;
-        setFormData({...formData, [key]: value});
+    const usenavigate=useNavigate();
+
+    useEffect(()=>{
+    sessionStorage.clear();
+    },[]);
+
+    const ProceedLogin = (e) => {
+        e.preventDefault();
+        if (validate()) {
+      
+            fetch("http://localhost:8000/user/" + username).then((res) => {
+                return res.json();
+            }).then((resp) => {
+        
+                if (Object.keys(resp).length === 0) {
+                    toast.error('Please Enter valid username');
+                } else {
+                    if (resp.password === password) {
+                        toast.success('Success');
+                        sessionStorage.setItem('username',username);
+                        sessionStorage.setItem('userrole',resp.role);
+                        usenavigate('/menu')
+                    }else{
+                        toast.error('Please Enter valid credentials');
+                    }
+                }
+            }).catch((err) => {
+                toast.error('Login Failed due to :' + err.message);
+            });
+        }
     }
 
-    const navigate = useNavigate()
 
-    const handleSubmit = (event) => {
-        event.preventDefault()
-        navigate('/menu')
-
+    const validate = () => {
+        let result = true;
+        if (username === '' || username === null) {
+            result = false;
+            toast.warning('Please Enter Username');
+        }
+        if (password === '' || password === null) {
+            result = false;
+            toast.warning('Please Enter Password');
+        }
+        return result;
     }
-    const inputStyle = {
-        width:'100%',
-        borderRadius:'15px',
-        border:'1px solid black',
-        padding:'10px'
-    };
-
-    const formstyle = {
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        margin: '70px auto',
-        backgroundColor: '#2C5F2D',
-        height: '50vh',
-        justifyContent: 'center',
-        width: '40%',
-        borderRadius: '10px'
-    };
-
-    const buttonStyle = {
-        borderRadius: '10px',
-        width: '20%'
-    };
-
     return (
-        <div>
-            <Form style={formstyle}>
-                <h1>Sign In</h1>
-                <Form.Group className="mb-3" controlId="formemail">
-                    <Form.Control style={inputStyle} type="email" placeholder="Email" id="email" value={formData.email} onChange={handleChange}></Form.Control>
-                </Form.Group>
-
-                <Form.Group className="mb-3" controlId="formpassword">
-                    <Form.Control style={inputStyle} type="password" placeholder="Password" id="password" value={formData.password} onChange={handleChange}></Form.Control>
-                </Form.Group>
-
-                <Button type="submit" style={buttonStyle} onClick={handleSubmit}>
-                    Sign In
-                </Button>
-            </Form>
+        <div className="row">
+            <div className="offset-lg-3 col-lg-6" style={{ marginTop: '100px' }}>
+                <form onSubmit={ProceedLogin} className="container">
+                    <div className="card">
+                        <div className="card-header">
+                            <h2>User Login</h2>
+                        </div>
+                        <div className="card-body">
+                            <div className="form-group">
+                                <label>User Name <span className="errmsg">*</span></label>
+                                <input value={username} onChange={e => usernameupdate(e.target.value)} className="form-control"></input>
+                            </div>
+                            <div className="form-group">
+                                <label>Password <span className="errmsg">*</span></label>
+                                <input type="password" value={password} onChange={e => passwordupdate(e.target.value)} className="form-control"></input>
+                            </div>
+                        </div>
+                        <div className="card-footer">
+                            <button type="submit" className="btn btn-primary">Login</button> |
+                            <Link className="btn btn-success" to={'/create_account'}>New User</Link>
+                        </div>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 }
 
-export default SignIn;
+export default SignIn
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useState } from "react";
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import { Button, Form } from "react-bootstrap";
+// import {useNavigate} from 'react-router-dom'
+
+// function SignIn(){
+//     const [formData, setFormData] = useState({
+//         email:"",
+//         password:""
+//     })
+
+//     const handleChange = (event) => {
+//         const key = event.target.id;
+//         const value = event.target.value;
+//         setFormData({...formData, [key]: value});
+//     }
+
+//     const navigate = useNavigate()
+
+//     const handleSubmit = (event) => {
+//         event.preventDefault()
+//         navigate('/menu')
+
+//     }
+//     const inputStyle = {
+//         width:'100%',
+//         borderRadius:'15px',
+//         border:'1px solid black',
+//         padding:'10px'
+//     };
+
+//     const formstyle = {
+//         display: 'flex',
+//         flexDirection: 'column',
+//         alignItems: 'center',
+//         margin: '70px auto',
+//         backgroundColor: '#2C5F2D',
+//         height: '50vh',
+//         justifyContent: 'center',
+//         width: '40%',
+//         borderRadius: '10px'
+//     };
+
+//     const buttonStyle = {
+//         borderRadius: '10px',
+//         width: '20%'
+//     };
+
+//     return (
+//         <div>
+//             <Form style={formstyle}>
+//                 <h1>Sign In</h1>
+//                 <Form.Group className="mb-3" controlId="formemail">
+//                     <Form.Control style={inputStyle} type="email" placeholder="Email" id="email" value={formData.email} onChange={handleChange}></Form.Control>
+//                 </Form.Group>
+
+//                 <Form.Group className="mb-3" controlId="formpassword">
+//                     <Form.Control style={inputStyle} type="password" placeholder="Password" id="password" value={formData.password} onChange={handleChange}></Form.Control>
+//                 </Form.Group>
+
+//                 <Button type="submit" style={buttonStyle} onClick={handleSubmit}>
+//                     Sign In
+//                 </Button>
+//             </Form>
+//         </div>
+//     );
+// }
+
+// export default SignIn;
--- Client/src/Components/SignUp.js
+++ Client/src/Components/SignUp.js
@@ -1,80 +1,245 @@
-import React, { useState } from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button, Form } from 'react-bootstrap';
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
-function SignUp() {
-    const [formData, setFormData] = useState({
-        firstName: '',
-        lastName: '',
-        email: '',
-        phoneNumber: '',
-        createPassword: '',
-        confirmPassword: ''
-    });
+const Register = () => {
 
-    const handleChange = (event) => {
-        const key = event.target.id;
-        const value = event.target.value;
-        setFormData({ ...formData, [key]: value });
-    };
+    const [id, idchange] = useState("");
+    const [name, namechange] = useState("");
+    const [password, passwordchange] = useState("");
+    const [email, emailchange] = useState("");
+    const [phone, phonechange] = useState("");
+  
+    const navigate = useNavigate();
 
-    const inputStyle = {
-        borderRadius: '10px',
-        border: '1px solid black',
-        padding: '10px',
-        width: '100%',
-    };
+    const IsValidate = () => {
+        let isproceed = true;
+        let errormessage = 'Please enter the value in ';
+        if (id === null || id === '') {
+            isproceed = false;
+            errormessage += ' Username';
+        }
+        if (name === null || name === '') {
+            isproceed = false;
+            errormessage += ' Fullname';
+        }
+        if (password === null || password === '') {
+            isproceed = false;
+            errormessage += ' Password';
+        }
+        if (email === null || email === '') {
+            isproceed = false;
+            errormessage += ' Email';
+        }
 
-    const formStyle = {
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '70vh',
-        flexDirection: 'column',
-        margin: '100px auto',
-        width: '40%',
-        backgroundColor: '#CDB599FF',
-        borderRadius: '20px',
-    };
+        if(!isproceed){
+            toast.warning(errormessage)
+        }else{
+            if(/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/.test(email)){
 
-    const buttonStyle = {
-        borderRadius: '10px',
-        marginTop: '10px',
-    };
+            }else{
+                isproceed = false;
+                toast.warning('Please enter the valid email')
+            }
+        }
+        return isproceed;
+    }
 
+
+    const handlesubmit = (e) => {
+            e.preventDefault();
+            let regobj = { id, name, password, email, phone};
+            if (IsValidate()) {
+            //console.log(regobj);
+            fetch("http://localhost:8000/user", {
+                method: "POST",
+                headers: { 'content-type': 'application/json' },
+                body: JSON.stringify(regobj)
+            }).then((res) => {
+                toast.success('Registered successfully.')
+                navigate('/sign_in');
+            }).catch((err) => {
+                toast.error('Failed :' + err.message);
+            });
+        }
+    }
     return (
         <div>
-            <Form style={formStyle}>
-                <h1>Create Account</h1>
-                <Form.Group className="mb-3" controlId="firstName">
-                    <Form.Control style={inputStyle} type="text" placeholder="First Name" value={formData.firstName} onChange={handleChange}></Form.Control>
-                </Form.Group>
+            <div className="offset-lg-3 col-lg-6">
+                <form className="container" onSubmit={handlesubmit}>
+                    <div className="card">
+                        <div className="card-header">
+                            <h1>User Registeration</h1>
+                        </div>
+                        <div className="card-body">
 
-                <Form.Group className="mb-3" controlId="lastName">
-                    <Form.Control style={inputStyle} type="text" placeholder="Last Name" value={formData.lastName} onChange={handleChange}></Form.Control>
-                </Form.Group>
+                            <div className="row">
+                                <div className="col-lg-6">
+                                    <div className="form-group">
+                                        <label>User Name <span className="errmsg">*</span></label>
+                                        <input value={id} onChange={e => idchange(e.target.value)} className="form-control"></input>
+                                    </div>
+                                </div>
+                                <div className="col-lg-6">
+                                    <div className="form-group">
+                                        <label>Password <span className="errmsg">*</span></label>
+                                        <input value={password} onChange={e => passwordchange(e.target.value)} type="password" className="form-control"></input>
+                                    </div>
+                                </div>
+                                <div className="col-lg-6">
+                                    <div className="form-group">
+                                        <label>Full Name <span className="errmsg">*</span></label>
+                                        <input value={name} onChange={e => namechange(e.target.value)} className="form-control"></input>
+                                    </div>
+                                </div>
+                                <div className="col-lg-6">
+                                    <div className="form-group">
+                                        <label>Email <span className="errmsg">*</span></label>
+                                        <input value={email} onChange={e => emailchange(e.target.value)} className="form-control"></input>
+                                    </div>
+                                </div>
+                                <div className="col-lg-6">
+                                    <div className="form-group">
+                                        <label>Phone <span className="errmsg"></span></label>
+                                        <input value={phone} onChange={e => phonechange(e.target.value)} className="form-control"></input>
+                                    </div>
+                                </div>
+                               
+                            </div>
 
-                <Form.Group className="mb-3" controlId="email">
-                    <Form.Control style={inputStyle} type="email" placeholder="Email" value={formData.email} onChange={handleChange}></Form.Control>
-                </Form.Group>
+                        </div>
+                        <div className="card-footer">
+                            <button type="submit" className="btn btn-primary">Register</button> |
+                            <Link to={'/sign_in'} className="btn btn-danger">Close</Link>
+                        </div>
+                    </div>
+                </form>
+            </div>
 
-                <Form.Group className="mb-3" controlId="phoneNumber">
-                    <Form.Control style={inputStyle} type="number" placeholder="Phone Number" value={formData.phoneNumber} onChange={handleChange}></Form.Control>
-                </Form.Group>
 
-                <Form.Group className="mb-3" controlId="createPassword">
-                    <Form.Control style={inputStyle} type="password" placeholder="Create Password" value={formData.createPassword} onChange={handleChange}></Form.Control>
-                </Form.Group>
-
-                <Form.Group className="mb-3" controlId="confirmPassword">
-                    <Form.Control style={inputStyle} type="password" placeholder="Confirm Password" value={formData.confirmPassword} onChange={handleChange}></Form.Control>
-                </Form.Group>
-
-                <Button style={buttonStyle}>Sign in with Google</Button> &nbsp;
-                <Button style={buttonStyle}>Sign in with Facebook</Button>
-            </Form>
         </div>
     );
 }
 
-export default SignUp;
+export default Register;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useState } from "react";
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import { Button, Form } from 'react-bootstrap';
+
+// function SignUp() {
+//     const [formData, setFormData] = useState({
+//         firstName: '',
+//         lastName: '',
+//         email: '',
+//         phoneNumber: '',
+//         createPassword: '',
+//         confirmPassword: ''
+//     });
+
+//     const handleChange = (event) => {
+//         const key = event.target.id;
+//         const value = event.target.value;
+//         setFormData({ ...formData, [key]: value });
+//     };
+
+//     const inputStyle = {
+//         borderRadius: '10px',
+//         border: '1px solid black',
+//         padding: '10px',
+//         width: '100%',
+//     };
+
+//     const formStyle = {
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         height: '70vh',
+//         flexDirection: 'column',
+//         margin: '100px auto',
+//         width: '40%',
+//         backgroundColor: '#CDB599FF',
+//         borderRadius: '20px',
+//     };
+
+//     const buttonStyle = {
+//         borderRadius: '10px',
+//         marginTop: '10px',
+//     };
+
+//     return (
+//         <div>
+//             <Form style={formStyle}>
+//                 <h1>Create Account</h1>
+//                 <Form.Group className="mb-3" controlId="firstName">
+//                     <Form.Control style={inputStyle} type="text" placeholder="First Name" value={formData.firstName} onChange={handleChange}></Form.Control>
+//                 </Form.Group>
+
+//                 <Form.Group className="mb-3" controlId="lastName">
+//                     <Form.Control style={inputStyle} type="text" placeholder="Last Name" value={formData.lastName} onChange={handleChange}></Form.Control>
+//                 </Form.Group>
+
+//                 <Form.Group className="mb-3" controlId="email">
+//                     <Form.Control style={inputStyle} type="email" placeholder="Email" value={formData.email} onChange={handleChange}></Form.Control>
+//                 </Form.Group>
+
+//                 <Form.Group className="mb-3" controlId="phoneNumber">
+//                     <Form.Control style={inputStyle} type="number" placeholder="Phone Number" value={formData.phoneNumber} onChange={handleChange}></Form.Control>
+//                 </Form.Group>
+
+//                 <Form.Group className="mb-3" controlId="createPassword">
+//                     <Form.Control style={inputStyle} type="password" placeholder="Create Password" value={formData.createPassword} onChange={handleChange}></Form.Control>
+//                 </Form.Group>
+
+//                 <Form.Group className="mb-3" controlId="confirmPassword">
+//                     <Form.Control style={inputStyle} type="password" placeholder="Confirm Password" value={formData.confirmPassword} onChange={handleChange}></Form.Control>
+//                 </Form.Group>
+
+//                 <Button style={buttonStyle}>Sign in with Google</Button> &nbsp;
+//                 <Button style={buttonStyle}>Sign in with Facebook</Button>
+//             </Form>
+//         </div>
+//     );
+// }
+
+// export default SignUp;
