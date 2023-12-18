8658d6fafb27da8e5cb8073aec335e2796ed3ac7
Laleti Cynthia <+>
2023-12-18 18:13:19 +0300

    Homepage final

--- Client/src/Components/HomePage.js
+++ Client/src/Components/HomePage.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
 import NavBar from './navbar';
-import { FiShoppingCart } from "react-icons/fi";
+import { FiShoppingCart } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 import '../App.css';
 
 function HomePage() {
   const [italianFoods, setItalianFoods] = useState([]);
   const [searchQuery, setSearchQuery] = useState('');
+  const [cart, setCart] = useState([]);
 
   const handleInputChange = (e) => {
     setSearchQuery(e.target.value);
@@ -27,6 +28,10 @@ function HomePage() {
         setItalianFoods(data);
       });
   }, []);
+
+  const handleAddToCart = (food) => {
+    setCart([...cart, food]);
+  };
 
   return (
     <div className='home-container'>
@@ -49,16 +54,20 @@ function HomePage() {
               <img className='images' src={food.image} alt={food.name} />
               <h3>{food.name}</h3>
               <p>{food.description}</p>
+              <Link to='./cart'>
+              <button className='add-to-cart-button' onClick={() => handleAddToCart(food)}>
+                Order
+                <FiShoppingCart />
+              </button>
+              </Link>
             </div>
           ))}
         </div>
       </div>
       <div className='cart'>
-        <Link to='/MyOrder' className='cart-link'>
-          <FiShoppingCart />
-        </Link> 
-
-        <span className='cart-quantity'>4</span>
+        <Link to='./cart' className='cart-link'>
+          <FiShoppingCart />Cart({cart.length})
+        </Link>
       </div>
     </div>
   );
