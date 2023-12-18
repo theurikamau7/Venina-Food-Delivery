167856c636fb5f7f37c7d54ec9e9508c4e78d333
Laleti Cynthia <+>
2023-12-18 12:12:20 +0300

    Lnading page css update

--- Client/src/Components/LandingPage.js
+++ Client/src/Components/LandingPage.js
@@ -1,18 +1,15 @@
 // LandingPage.js
 import React from 'react';
-import { Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { Button } from 'react-bootstrap'; // Correct import statement for Button from react-bootstrap
 import './LandingPage.css';
 
 function LandingPage() {
   return (
-    <div className="landing-page-container">
-      {/* Left Column - Central Image */}
-      <div className="central-image-container">
-        <img src='/Venina.png' alt='Italian Food' className="central-image" />
+    <div className='landing-page'>
+      <div className="logo">
+        <img src='/Venina.png' alt="Venina Logo" />
       </div>
-
-      {/* Right Column - Order Your Italian Button */}
       <div className="order-button-container">
         <Link to='/sign_in_up'>
           <Button className="order-button" variant='primary'>
